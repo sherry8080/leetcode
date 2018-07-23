1e916a5333fa4f7cf52af2f911a3ba050085c31c
fan_2017@outlook.com <fan_2017@outlook.com>
2018-07-23 16:27:33 +0800

    7/23

---
+++ src/string/CustomSortString.java
@@ -0,0 +1,63 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
+
+S was sorted in some custom order previously. We want to permute the characters of T so that they match the order that S was sorted. More specifically, if x occurs before y in S, then x should occur before y in the returned string.
+
+Return any permutation of T (as a string) that satisfies this property.
+
+Example :
+Input: 
+S = "cba"
+T = "abcd"
+Output: "cbad"
+Explanation: 
+"a", "b", "c" appear in S, so the order of "a", "b", "c" should be "c", "b", and "a". 
+Since "d" does not appear in S, it can be at any position in T. "dcba", "cdba", "cbda" are also valid outputs.
+ 
+
+Note:
+
+S has length at most 26, and no character is repeated in S.
+T has length at most 200.
+S and T consist of lowercase letters only.*/
+
+public class CustomSortString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(customSortString("cba", "fcccabcdeeeef"));
+	}
+	
+	public static String customSortString(String S, String T) {
+        StringBuffer sb = new StringBuffer();
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c: T.toCharArray()) {
+        	if(!map.containsKey(c)){
+        		map.put(c, 1);
+        	}else{
+        		map.put(c, map.get(c) + 1);
+        	}
+        }
+        for(char c: S.toCharArray()) {
+        	if(map.containsKey(c)){
+        		while(map.get(c) > 0){
+        			sb.append(c);
+        			map.put(c, map.get(c) - 1);
+        		}
+        		map.remove(c);
+        	}
+        }
+        for(char c: T.toCharArray()){
+        	if(map.containsKey(c)){
+        		sb.append(c);
+        	}
+        }
+        
+		return sb.toString();
+    }
+
+}
