33704243f459cc527f6dd87927585f2240a2511b
fan_2017@outlook.com <fan_2017@outlook.com>
2018-07-27 17:01:40 +0800

    description

--- src/string/Vowels.java
+++ src/string/Vowels.java
@@ -1,7 +1,17 @@
 package string;
 
-import java.util.HashMap;
 import java.util.HashSet;
+
+/*Write a function that takes a string as input and reverse only the vowels of a string.
+
+        Example 1:
+        Given s = "hello", return "holle".
+
+        Example 2:
+        Given s = "leetcode", return "leotcede".
+
+        Note:
+        The vowels does not include the letter "y".*/
 
 public class Vowels {
     public static void main(String args[]){
